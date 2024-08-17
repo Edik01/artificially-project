a98bc3d5c5a3f57e9cf8f32c34c0a081392c22f8
Edik Huloian <guloyan.edik@gmail.com>
2024-08-16 19:51:49 -0700

    update

--- app.tsx
+++ app.tsx
@@ -3,8 +3,13 @@ import React, { useState } from "react";
 const App: React.FC = () => {
   const [direction, setDirection] = useState<string>("None");
 
+  const [speed, setSpeed] = useState<number>(1);
+
   const handleDirectionChange = (newDirection: string) => {
     setDirection(newDirection);
     console.log(`Drone moving ${newDirection}`);
+  };
+  const handleSpeedChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSpeed(Number(event.target.value));
   };
 };
