fc9241d4604b3f9828942ad4ba9702d4460a4430
Edik Huloian <guloyan.edik@gmail.com>
2024-09-11 10:25:15 -0700

    update

--- dron.tsx
+++ dron.tsx
@@ -1,3 +1,8 @@
 import React, { useState, useRef } from "react";
 import * as mobilenet from "@tensorflow-models/mobilenet";
 import "@tensorflow/tfjs";
+
+const DroneEnemyDetection: React.FC = () => {
+  const [predictions, setPredictions] = useState<string[]>([]);
+  const videoRef = useRef<HTMLVideoElement>;
+};
