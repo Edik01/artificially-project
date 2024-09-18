38423a6427ce8bca9c21bb0dd8c672c29c235f96
Edik Huloian <guloyan.edik@gmail.com>
2024-09-18 10:46:02 -0700

    update

--- dron.tsx
+++ dron.tsx
@@ -5,4 +5,10 @@ import "@tensorflow/tfjs";
 const DroneEnemyDetection: React.FC = () => {
   const [predictions, setPredictions] = useState<string[]>([]);
   const videoRef = useRef<HTMLVideoElement>(null);
+  let login = armyCod;
+  if (armyCod >= access) {
+    console.log("you have access cod.");
+  } else {
+    console.log("Your access cod incomplete");
+  }
 };
