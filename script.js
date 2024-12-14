5658febbf46694c85933384c76462182256b999d
Edik Huloian <guloyan.edik@gmail.com>
2024-12-13 18:37:59 -0800

    sumarry

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
   const button = document.querySelector("button");
   button.addEventListener("click", changeMessage);
+
+  const faithingButton = document.getElementById("faithingButton");
+  faithingButton.addEventListener("click", function () {
+    window.location.href = "faithingPage.html";
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Логика кнопки "Back to Home"
+  const backToHomeButton = document.getElementById("backToHomeButton");
+  backToHomeButton.addEventListener("click", function () {
+    window.location.href = "index.html"; // Путь к домашней странице
+  });
 });
 
 function changeMessage() {
